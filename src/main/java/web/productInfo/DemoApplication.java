b8ae913547697357dd78baa3f236c595b1822478
Art3Z10u5 <art3z10u5@icloud.com>
2017-01-06 02:39:36 +0200

    fix #1

--- src/main/java/web/productInfo/DemoApplication.java
+++ src/main/java/web/productInfo/DemoApplication.java
@@ -1,19 +1,14 @@
 package web.productInfo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import web.productInfo.repository.ProductRepository;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
 public class DemoApplication {
-
-	@Autowired
-	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
